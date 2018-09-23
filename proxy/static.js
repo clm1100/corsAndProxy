6f31a053603a39f2633df78b64505dd6cbd04667
clm110 <275298711@qq.com>
2018-09-24 01:12:20 +0800

    修改proxy

--- proxy/static.js
+++ proxy/static.js
@@ -3,11 +3,11 @@ var proxy = require('http-proxy-middleware');
 var app = express();
 app.use(express.static('./public'));
  
-// app.use('/api', proxy({ 
-//     target: 'http://localhost:5000',
-//     changeOrigin: false,
-//     pathRewrite:{
-//         "^/api":""
-//     }
-// }));
+app.use('/api', proxy({ 
+    target: 'http://localhost:5000',
+    changeOrigin: false,
+    pathRewrite:{
+        "^/api":""
+    }
+}));
 app.listen(3000);
